de55fdc5d5c96115328710faa446cfa6279f84f1
ShawnXxy <shawnxxy@hotmail.com>
2017-07-20 12:53:17 +0800

    initializing game

--- scripts/main.js
+++ scripts/main.js
@@ -3,13 +3,14 @@ $(function() {
 });
 
 function newgame() {
-    // initializing
+    // initializing game board
     init();
     // generate two number randomly in two cells
     generateOneNumber();
     generateTwoNumber();
 }
 
+// game board
 var board = new Array();
 function init() {
     for (var i = 0; i < 4; i++) { // for rows
@@ -18,9 +19,25 @@ function init() {
             board[i][j] = 0; // initializing each cell to 0
             var gridCell = $("#grid-cell-" + i + "-" + j);
             // set margin-top for each cell
-            gridCell.css("top", getPostTop(i, j));
+            gridCell.css("top", getPosTop(i, j));
             // set margin-left for each cell
-            gridCell.css("left", getPostLeft(i, j));
+            gridCell.css("left", getPosLeft(i, j));
+        }
+    }
+    updateBoardView();
+}
+function getPosTop(i, j) {
+    return 20 +  i * 120; // 20px for margin, 
+}
+function getPosLeft(i, j) {
+    return 20 + j * 120;
+}
+// game cell
+function updateBoardView() {
+    for (var i = 0; i < 4; i++) {
+        for (var j = 0; j < 4; j++) {
+            $("#grid-container").append("<div class='number-cell' id='number-cell-" + i + "-" + j +"'></div>");
+            
         }
     }
 }
